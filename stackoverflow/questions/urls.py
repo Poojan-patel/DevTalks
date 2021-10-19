c28d86e3e581677059c9149f2ba97ad3533b051c
Poojan Patel <poojan4004@gmail.com>
2021-10-19 12:27:30 +0530

    Editor Js plugged in

--- stackoverflow/questions/urls.py
+++ stackoverflow/questions/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from questions import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
      path('read/<uuid>/',views.read,name='read'),
      path('read/',views.readall,name='readall'),
      path('question/',views.add_question,name='add_question'),
-     path('feed/',views.get_feed,name='feed')
+     path('feed/',views.get_feed,name='feed'),
+     path('editor/',TemplateView.as_view(template_name='editor.html'))
 ]
