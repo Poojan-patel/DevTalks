22ad2f8a226d67f2d792dca59d3c464ff322b037
Shubh-Official <shubh_official@icloud.com>
2021-11-02 13:04:07 +0530

    stackoverflow directory removed.

--- stackoverflow/questions/templatetags/custom_filter.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name="find_user")
-def find_user(query_set, user):
-    return query_set.filter(user=user).exists()
-    # return True
--- stackoverflow/stackoverflow/errorview.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import messages
-from django.shortcuts import redirect
-
-def errorView(request):
-     #messages.error(request,"Request Page not Exists")
-     messages.warning(request,"Requested Page not Exists, Redirected to Home")
-     #messages.info(request,"Redirected to Feed")
-     return redirect('feed')
